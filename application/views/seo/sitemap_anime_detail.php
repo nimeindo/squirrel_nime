42ea4602f54dbe920a452dd21c05c3f81358f7bc
Prayugo Dwi <pray.arsian24@gmail.com>
2020-09-02 23:27:42 +0700

    update sitemap

--- application/views/seo/sitemap_anime_detail.php
+++ application/views/seo/sitemap_anime_detail.php
@@ -1,11 +1,9 @@
 <?php 
-header('Content-type: application/xml; charset="ISO-8859-1"',true);  
+// header('Content-type: application/xml; charset="ISO-8859-1"',true);  
 
 ?>
-<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" 
-    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <?php foreach($API_ListAnime->API_TheMovieRs->Body->ListAnime as $ListAnime){ ?>
         <?php foreach($ListAnime->ListSubIndex as $ListSubIndex){ ?>
             <?php //$params = $ListSubIndex->IdDetailAnime.'-'.$ListSubIndex->SlugDetail;?>
